b98e00e781636061e378d7fbfc71392b78549d67
Alisson Silva <camargoalisson92@hotmail.com>
2025-02-07 07:20:21 -0300

    Chamar chat gpt

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("Inserir sua key aqui");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
